8277880b83b5604359624001f90b84e1d392d743
bhughes <bhughes@d1478e1f-098f-4d0d-a7d8-6200410a3e89>
2008-04-12 00:55:49 +0000

    Fixed Production Database Configuration
    git-svn-id: http://svn.railsgarden.com/projects/goplay/trunk@45 d1478e1f-098f-4d0d-a7d8-6200410a3e89

--- app/helpers/moves_helper.rb
+++ app/helpers/moves_helper.rb
@@ -1,5 +1,5 @@
 module Merb
-  module MofsHelper
+  module MovesHelper
     
   end
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,52 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "goplay"
+set :repository, "http://svn.railsgarden.com/projects/#{application}/trunk"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :scm, :subversion
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+task :production do
+	role :web, "cedar.synenterprises.com"
+	role :app, "cedar.synenterprises.com"
+	role :db,  "cedar.synenterprises.com", :primary => true
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+	set :rails_env, "production"
+	set :deploy_to, "/var/www/goplay.rubiety.com/app"
+	set :user, "bhughes"
+	set :use_sudo, false
+end
+
+namespace :deploy do
+  task :restart do
+	  restart_merb
+	end
+	
+	task :start do
+		start_merb
+	end
+	
+	task :stop do
+		stop_merb
+	end
+	
+	task :restart_merb do
+		stop_merb
+		start_merb
+	end
+	
+	task :start_merb do
+		run "merb -c 1 -L log/merb.log"
+	end
+	
+	task :stop_merb do
+		run "merb -K all"
+	end
+	
+end
+
+after "deploy:update_code", :create_permissions
+
+task :create_permissions do
+  run "find #{release_path}/public -type d -exec chmod 0755 {} \\;" 
+  run "find #{release_path}/public -type f -exec chmod 0644 {} \\;"
+  run "chmod 0755 #{release_path}/public/merb.fcgi" 
+end
+
